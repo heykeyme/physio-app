61ed8a13f1422324110161bcfd82e498168f430f
Hakimi Halim <hakimihalim21@gmail.com>
2026-06-14 13:29:55 +0800

    Initial commit: Setup spring boot project

---
+++ src/test/java/com/alpro/physio/PhysioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpro.physio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
